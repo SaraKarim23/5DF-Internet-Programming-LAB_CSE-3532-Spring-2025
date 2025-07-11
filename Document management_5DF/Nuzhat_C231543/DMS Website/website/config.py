9695f1ec8a2ee88f71d4865b58a8752ccfb56f24
NuzhatJacky25 <nuzhattabassum2504jacky@gmail.com>
2025-07-11 11:46:26 +0600

    Delete Document management_5DF/Nuzhat_C231543 directory

--- Document management_5DF/Nuzhat_C231543/DMS Website/website/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-UPLOAD_FOLDER = "uploads"
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "pdf", "doc", "docx"}
-
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
